9939e65cf8e73526f6870e1b5a88a00c9a802184
Kei Yasuda <adieuyaks@gmail.com>
2022-12-23 14:00:03 +0900

    added test_getitem.py into test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -179,6 +179,43 @@ class TestBroadcast(unittest.TestCase):
 # =============================================================================
 # =============================================================================
 # =============================================================================
+class TestGetitem(unittest.TestCase):
+
+    def test_forward1(self):
+        x_data = np.arange(12).reshape((2, 2, 3))
+        x = Variable(x_data)
+        y = get_item(x, 0)
+        self.assertTrue(array_allclose(y.data, x_data[0]))
+
+    def test_forward1a(self):
+        x_data = np.arange(12).reshape((2, 2, 3))
+        x = Variable(x_data)
+        y = x[0]
+        self.assertTrue(array_allclose(y.data, x_data[0]))
+
+    def test_forward2(self):
+        x_data = np.arange(12).reshape((2, 2, 3))
+        x = Variable(x_data)
+        y = get_item(x, (0, 0, slice(0, 2, 1)))
+        self.assertTrue(array_allclose(y.data, x_data[0, 0, 0:2:1]))
+
+    def test_forward3(self):
+        x_data = np.arange(12).reshape((2, 2, 3))
+        x = Variable(x_data)
+        y = get_item(x, (Ellipsis, 2))
+        self.assertTrue(array_allclose(y.data, x_data[..., 2]))
+
+    def test_backward1(self):
+        x_data = np.array([[1, 2, 3], [4, 5, 6]])
+        slices = 1
+        f = lambda x: get_item(x, slices)
+        gradient_check(f, x_data)
+
+    def test_backward2(self):
+        x_data = np.arange(12).reshape(4, 3)
+        slices = slice(1, 3)
+        f = lambda x: get_item(x, slices)
+        gradient_check(f, x_data)
 
 
 # =============================================================================
