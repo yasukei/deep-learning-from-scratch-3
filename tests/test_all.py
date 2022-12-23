896096c12a13f62d0eafa8c30d3aac3e8fa67723
Kei Yasuda <adieuyaks@gmail.com>
2022-12-23 13:51:40 +0900

    added test_matmul.py into test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -308,6 +308,27 @@ class TestMSE_simple(unittest.TestCase):
 # =============================================================================
 # =============================================================================
 # =============================================================================
+class TestMatmul(unittest.TestCase):
+
+    def test_forward1(self):
+        x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+        w = Variable(x.data.T)
+        y = matmul(x, w)
+        res = y.data
+        expected = np.array([[14, 32], [32, 77]])
+        self.assertTrue(array_allclose(res, expected))
+
+    def test_backward1(self):
+        x = np.random.randn(3, 2)
+        w = np.random.randn(2, 3)
+        f = lambda x: matmul(x, Variable(w))
+        self.assertTrue(gradient_check(f, x))
+
+    def test_backward2(self):
+        x_data = np.random.randn(10, 1)
+        w_data = np.random.randn(1, 5)
+        f = lambda w: matmul(Variable(x_data), w)
+        self.assertTrue(gradient_check(f, w_data))
 
 
 # =============================================================================
