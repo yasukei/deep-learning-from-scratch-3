9e7a249f47ca8ad481b9ab3800960fcdc27ba8e0
Kei Yasuda <adieuyaks@gmail.com>
2022-12-23 15:15:40 +0900

    added test_relu.py into test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -829,6 +829,58 @@ class TestAveragePooling(unittest.TestCase):
 # =============================================================================
 # =============================================================================
 # =============================================================================
+class TestRelu(unittest.TestCase):
+
+    def test_forward1(self):
+        x = np.array([[-1, 0], [2, -3], [-2, 1]], np.float32)
+        res = relu(x)
+        ans = np.array([[0, 0], [2, 0], [0, 1]], np.float32)
+        self.assertTrue(array_allclose(res, ans))
+
+    def test_backward1(self):
+        x_data = np.array([[-1, 1, 2], [-1, 2, 4]])
+        self.assertTrue(gradient_check(relu, x_data))
+
+    def test_backward2(self):
+        np.random.seed(0)
+        x_data = np.random.rand(10, 10) * 100
+        self.assertTrue(gradient_check(relu, x_data))
+
+    def test_backward3(self):
+        np.random.seed(0)
+        x_data = np.random.rand(10, 10, 10) * 100
+        self.assertTrue(gradient_check(relu, x_data))
+
+
+class TestLeakyRelu(unittest.TestCase):
+
+    def test_forward1(self):
+        x = np.array([[-1, 0], [2, -3], [-2, 1]], np.float32)
+        res = leaky_relu(x)
+        ans = np.array([[-0.2, 0.], [2., -0.6], [-0.4, 1.]], np.float32)
+        self.assertTrue(array_allclose(res, ans))
+
+    def test_forward2(self):
+        slope = 0.002
+        x = np.random.randn(100)
+        y2 = CF.leaky_relu(x, slope)
+        y = leaky_relu(x, slope)
+        res = array_allclose(y.data, y2.data)
+        self.assertTrue(res)
+
+    def test_backward1(self):
+        x_data = np.array([[-1, 1, 2], [-1, 2, 4]])
+        self.assertTrue(gradient_check(leaky_relu, x_data))
+
+    def test_backward2(self):
+        np.random.seed(0)
+        x_data = np.random.rand(10, 10) * 100
+        self.assertTrue(gradient_check(leaky_relu, x_data))
+
+    def test_backward3(self):
+        np.random.seed(0)
+        x_data = np.random.rand(10, 10, 10) * 100
+        self.assertTrue(gradient_check(leaky_relu, x_data))
 
 
 # =============================================================================
