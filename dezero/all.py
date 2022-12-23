24a6846272274a819e3c8d8c1a14a5829c67f0ff
Kei Yasuda <adieuyaks@gmail.com>
2022-12-23 12:45:04 +0900

    deleted some codes in transforms.py because 'import Image' fails in atcoder

--- dezero/all.py
+++ dezero/all.py
@@ -533,12 +533,6 @@ class SeqDataLoader(DataLoader):
 # =============================================================================
 # =============================================================================
 # =============================================================================
-try:
-    import Image
-except ImportError:
-    from PIL import Image
-
-
 class Compose:
     """Compose several transforms.
 
@@ -554,86 +548,6 @@ class Compose:
         for t in self.transforms:
             img = t(img)
         return img
-
-
-# =============================================================================
-# Transforms for PIL Image
-# =============================================================================
-class Convert:
-    def __init__(self, mode='RGB'):
-        self.mode = mode
-
-    def __call__(self, img):
-        if self.mode == 'BGR':
-            img = img.convert('RGB')
-            r, g, b = img.split()
-            img = Image.merge('RGB', (b, g, r))
-            return img
-        else:
-            return img.convert(self.mode)
-
-
-class Resize:
-    """Resize the input PIL image to the given size.
-
-    Args:
-        size (int or (int, int)): Desired output size
-        mode (int): Desired interpolation.
-    """
-    def __init__(self, size, mode=Image.BILINEAR):
-        self.size = pair(size)
-        self.mode = mode
-
-    def __call__(self, img):
-        return img.resize(self.size, self.mode)
-
-
-class CenterCrop:
-    """Resize the input PIL image to the given size.
-
-    Args:
-        size (int or (int, int)): Desired output size.
-        mode (int): Desired interpolation.
-    """
-    def __init__(self, size):
-        self.size = pair(size)
-
-    def __call__(self, img):
-        W, H = img.size
-        OW, OH = self.size
-        left = (W - OW) // 2
-        right = W - ((W - OW) // 2 + (W - OW) % 2)
-        up = (H - OH) // 2
-        bottom = H - ((H - OH) // 2 + (H - OH) % 2)
-        return img.crop((left, up, right, bottom))
-
-
-class ToArray:
-    """Convert PIL Image to NumPy array."""
-    def __init__(self, dtype=np.float32):
-        self.dtype = dtype
-
-    def __call__(self, img):
-        if isinstance(img, np.ndarray):
-            return img
-        if isinstance(img, Image.Image):
-            img = np.asarray(img)
-            img = img.transpose(2, 0, 1)
-            img = img.astype(self.dtype)
-            return img
-        else:
-            raise TypeError
-
-
-class ToPIL:
-    """Convert NumPy array to PIL Image."""
-    def __call__(self, array):
-        data = array.transpose(1, 2, 0)
-        return Image.fromarray(data)
-
-
-class RandomHorizontalFlip:
-    pass
 
 
 # =============================================================================
